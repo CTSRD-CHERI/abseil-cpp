045e3a9de3698a973f7f8afbd389756d81550dc1
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2023-05-02 09:54:58 +0000

    [cheri] Use the correct constants for Cord buffer capacity in tests.

--- absl/strings/cord_test.cc
+++ absl/strings/cord_test.cc
@@ -763,7 +763,11 @@ TEST_P(CordTest, AppendEmptyBufferToTree) {
 TEST_P(CordTest, AppendSmallBuffer) {
   absl::Cord cord;
   absl::CordBuffer buffer = absl::CordBuffer::CreateWithDefaultLimit(3);
+#if defined(__CHERI_PURE_CAPABILITY__)
+  ASSERT_THAT(buffer.capacity(), Le(31));
+#else
   ASSERT_THAT(buffer.capacity(), Le(15));
+#endif
   memcpy(buffer.data(), "Abc", 3);
   buffer.SetLength(3);
   cord.Append(std::move(buffer));
@@ -816,7 +820,11 @@ TEST_P(CordTest, AppendAndPrependBufferArePrecise) {
 TEST_P(CordTest, PrependSmallBuffer) {
   absl::Cord cord;
   absl::CordBuffer buffer = absl::CordBuffer::CreateWithDefaultLimit(3);
+#if defined(__CHERI_PURE_CAPABILITY__)
+  ASSERT_THAT(buffer.capacity(), Le(31));
+#else
   ASSERT_THAT(buffer.capacity(), Le(15));
+#endif
   memcpy(buffer.data(), "Abc", 3);
   buffer.SetLength(3);
   cord.Prepend(std::move(buffer));
