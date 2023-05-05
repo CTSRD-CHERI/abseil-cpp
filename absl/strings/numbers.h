b8fe239274a54bcb7d76f8c9057523ca565649a9
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2023-05-05 10:57:30 +0000

    [cheri] Fix additional static assert in safe_strtoi_base.
    (cherry picked from commit 55a607c66c616ff8f58739ef4abe5e13be4b4636)

--- absl/strings/numbers.h
+++ absl/strings/numbers.h
@@ -244,11 +244,18 @@ char* absl_nonnull FastIntToBuffer(int_type i,
 // Implementation of SimpleAtoi, generalized to support arbitrary base (used
 // with base different from 10 elsewhere in Abseil implementation).
 template <typename int_type>
-[[nodiscard]] bool safe_strtoi_base(absl::string_view s,
-                                    int_type* absl_nonnull out, int base) {
+ABSL_MUST_USE_RESULT bool safe_strtoi_base(absl::string_view s, int_type* out,
+                                           int base) {
+#if defined(__CHERI_PURE_CAPABILITY__)
+  static_assert(sizeof(*out) == 1 || sizeof(*out) == 2 || sizeof(*out) == 4 ||
+                    sizeof(*out) == 8 || std::is_same<int_type, intptr_t>::value ||
+                    std::is_same<int_type, uintptr_t>::value,
+                "SimpleAtoi works only with 8, 16, 32, or 64-bit integers.");
+#else
   static_assert(sizeof(*out) == 1 || sizeof(*out) == 2 || sizeof(*out) == 4 ||
                     sizeof(*out) == 8,
                 "SimpleAtoi works only with 8, 16, 32, or 64-bit integers.");
+#endif
   static_assert(!std::is_floating_point<int_type>::value,
                 "Use SimpleAtof or SimpleAtod instead.");
   bool parsed;
